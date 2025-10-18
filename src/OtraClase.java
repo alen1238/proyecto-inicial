3ef275e73da674307cf4c1544fa41a448a526cfc
Alfonso Lara <orozcoenrique159@gmail.com>
2025-10-18 16:36:15 -0500

    versión base del proyecto

---
+++ src/OtraClase.java
@@ -0,0 +1,11 @@
+
+public class OtraClase {
+    // Esta es otra clase en el mismo archivo
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo Dev Java!");
+
+        System.out.println("Leyendo instrucción 2");
+        System.out.println("Leyendo instrucción 1");
+
+    }
+}
