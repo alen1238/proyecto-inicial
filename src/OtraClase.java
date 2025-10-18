256ad6cb6b6933bae21fb0bb1f83fac1f3235e5a
Alfonso Lara <orozcoenrique159@gmail.com>
2025-10-18 16:53:09 -0500

    agregado el feature de rama2

--- src/OtraClase.java
+++ src/OtraClase.java
@@ -3,9 +3,14 @@ public class OtraClase {
     // Esta es otra clase en el mismo archivo
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo Dev Java!");
-
         System.out.println("Leyendo instrucción 2");
         System.out.println("Leyendo instrucción 1");
 
+        main2();
+    }
+
+    public static void main2() {
+
+        System.out.println("Modificación desde rama feature");
     }
 }
